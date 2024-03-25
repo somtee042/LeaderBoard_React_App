ed4129410289563c58ebcc4a9825028d1ed024ae
somto <nwangwusomto2003@gmail.com>
2024-03-25 22:36:22 +0100

    initial commit

--- src/components/database.js
+++ src/components/database.js
@@ -1,27 +1,27 @@
 export const Leaderboard = [
     {
-        name: "ebube",
+        name: "Nzeh Favour",
         location: "enugu",
         score : 50,
         img: "https://tse2.mm.bing.net/th?id=OIP.Nu6BCEfAkiWV4DcNaCLsAwHaLy&pid=Api&P=0&h=220",
       
     },
     {
-        name: "chigozie favour",
+        name: "Chisom Opie",
         location: "imo",
-        score : 15,
+        score : 55,
         img: "https://tse2.mm.bing.net/th?id=OIP.Ck2Usj84bIh_SiI2QJ7b-QHaEp&pid=Api&P=0&h=220",
       
     },
     {
-        name: "ahm ibrahim",
+        name: "Kosiso odiwankpa",
         location: "lagos",
         score : 30,
         img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
     
     },
     {
-        name: "favour chisom",
+        name: "Somtochukwu Nwangwu",
         location: "nsukka",
         score : 100,
         img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
@@ -29,7 +29,7 @@ export const Leaderboard = [
     },
 
     {
-        name: "ngozi gabriel",
+        name: "Ngozi Gabriel",
         location: "abuja",
         score : 50,
         img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
@@ -37,9 +37,17 @@ export const Leaderboard = [
     },
 
     {
-        name: "somtoo ngwu",
+        name: "Gospel Chekwube",
         location: "onitsha",
         score : 80,
+        img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+   
+    },
+
+    {
+        name: "Somtochukwu Ozonnadi",
+        location: "Enugu",
+        score : 34,
         img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
    
     },
